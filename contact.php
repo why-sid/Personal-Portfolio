7d79bef1e0342befd2f04621b052282c989eb2e3
why-sid <sidchourasia13@gmail.com>
2024-06-28 02:39:00 +0530

    Merge branch 'master' of https://github.com/why-sid/Personal-Portfolio

--- contact.php
+++ contact.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = $_POST["name"];
@@ -22,28 +21,3 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     exit;
 }
 ?>
-=======
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = $_POST["name"];
-    $email = $_POST["email"];
-    $message = $_POST["message"];
-
-    // Set up email parameters
-    $to = "sidofficial1005@gmail.com";
-    $subject = "New message from your website";
-    $body = "Name: $name\n\nEmail: $email\n\nMessage:\n$message";
-
-    // Send email
-    if (mail($to, $subject, $body)) {
-        echo "Message sent successfully!";
-    } else {
-        echo "Error: Unable to send message.";
-    }
-} else {
-    // Redirect back to the contact form if accessed directly
-    header("Location: index.html");
-    exit;
-}
-?>
->>>>>>> bbed84ac4fb82982f05745a857bab33e38abe945
